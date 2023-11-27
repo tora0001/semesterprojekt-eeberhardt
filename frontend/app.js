4e4a195df3d449925887998c3c4961bc59dc5384
tora0001 <tobe.2309@hotmail.com>
2023-11-27 15:20:16 +0100

    added a bit of frontend to fetch employees

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+//brug dette endpoint for at teste med azure
+// const endpoint = "https://semesterprojekt-eeberhardt.azurewebsites.net";
+
+//brug dette endpoint for at teste lokalt
+const endpoint = "http://localhost:4000";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("running");
+  updateEmployeeGrid();
+}
+
+async function updateEmployeeGrid() {
+  const employees = await getEmployees();
+  showEmployees(employees);
+}
+
+async function showEmployees() {}
+
+async function getEmployees() {
+  const response = await fetch(`${endpoint}/employee`);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
