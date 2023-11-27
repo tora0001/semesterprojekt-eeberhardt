13bcf6b690d1a49aba9049517d7e1d2ec7ba3327
tora0001 <tobe.2309@hotmail.com>
2023-11-27 15:53:49 +0100

    updated frontend

--- frontend/app.js
+++ frontend/app.js
@@ -9,20 +9,5 @@ const endpoint = "http://localhost:4000";
 window.addEventListener("load", start);
 
 function start() {
-  console.log("running");
-  updateEmployeeGrid();
-}
-
-async function updateEmployeeGrid() {
-  const employees = await getEmployees();
-  showEmployees(employees);
-}
-
-async function showEmployees() {}
-
-async function getEmployees() {
-  const response = await fetch(`${endpoint}/employee`);
-  const data = await response.json();
-  console.log(data);
-  return data;
+  console.log("Running");
 }
