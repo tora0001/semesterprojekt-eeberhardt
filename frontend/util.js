8bade687465b40724d2dae7958d88f7231e8b03e
tora0001 <tobe.2309@hotmail.com>
2023-12-14 12:27:52 +0100

    Update util.js

--- frontend/util.js
+++ frontend/util.js
@@ -1,26 +1,26 @@
 function formatDate(inputDate) {
-    const dateObject = new Date(inputDate);
-    const year = dateObject.getFullYear();
-    const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const day = String(dateObject.getDate()).padStart(2, '0');
- 
-    return `${year}-${month}-${day}`;
- }
+  const dateObject = new Date(inputDate);
+  const year = dateObject.getFullYear();
+  const month = String(dateObject.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(dateObject.getDate()).padStart(2, "0");
 
- function refreshEmployeeList() {
-    fetch(`${endpoint}/employees`)
-       .then((response) => response.json())
-       .then((data) => {
-          employees = data;
-          populateEmployeeTable(employees);
-       });
- }
- 
- function refreshVacationList() {
-    fetch(`${endpoint}/vacations`)
-       .then((response) => response.json())
-       .then((data) => {
-          vacations = data;
-          populateVacationTable(vacations);
-       });
- }
+  return `${day}-${month}-${year}`;
+}
+
+function refreshEmployeeList() {
+  fetch(`${endpoint}/employees`)
+    .then((response) => response.json())
+    .then((data) => {
+      employees = data;
+      populateEmployeeTable(employees);
+    });
+}
+
+function refreshVacationList() {
+  fetch(`${endpoint}/vacations`)
+    .then((response) => response.json())
+    .then((data) => {
+      vacations = data;
+      populateVacationTable(vacations);
+    });
+}
